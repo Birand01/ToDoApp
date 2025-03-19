5cbc63d508c13ead02a8ff56dc656a39f19c337e
Birand Kılınç <pacmaidanapac@gmail.com>
2025-03-19 20:11:55 +0300

    Button and useState method is combined

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React,{useState} from 'react';
 
 export default function App() {
+  const[name,setName]=useState("Birand");
+  const[person,setPerson]=useState({name:"Mario",age:40});
+  const clickHandler=()=>
+  {
+    setName("Kilinc");
+    setPerson({name:"HAWAGI",age:45});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>DA RA LI YO asdsadRUM</Text>
-      <StatusBar style="auto" />
+    <Text>My name is {name}</Text>
+    <Text>His name is {person.name} and his age is {person.age}</Text>
+     <View style={styles.buttonContainer}>
+      <Button title="UpdateState" onPress={clickHandler}/>
+     </View>
     </View>
   );
 }
@@ -17,4 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer:
+  {
+    marginTop:20
+  }
 });
+ 
